fb3c47b656c9295ebac3426e3dbbe8eb7e8aedd3
NegotiationSkill <1648014891@qq.com>
2019-06-01 16:28:12 +0800

    init

---
+++ study_online-parent/study_online-manager/study_online-manager-dao/src/main/java/com/study_online/mapper/TestMapper.java
@@ -0,0 +1,10 @@
+package com.study_online.mapper;
+
+/**
+ * @author 刘棋军
+ * @date2019-05-28
+ */
+
+public interface TestMapper {
+    public String queryNow();
+}
---
+++ study_online-parent/study_online-manager/study_online-manager-interface/src/main/java/com/study_online/service/TestService.java
@@ -0,0 +1,11 @@
+package com.study_online.service;
+
+/**
+ * @author 刘棋军
+ * @date2019-05-28
+ */
+
+public interface TestService {
+    public String queryNow();
+
+}
