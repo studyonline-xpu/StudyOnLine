173a8b514c489336793dae69b7daf33a412493f7
NegotiationSkill <1648014891@qq.com>
2019-06-05 16:51:08 +0800

    目录查询api

---
+++ study_online-parent/study_online-manager-web/src/main/java/com/study_online/controller/CatalogController.java
@@ -0,0 +1,34 @@
+package com.study_online.controller;
+
+import com.study_online.pojo.Catalog;
+import com.study_online.service.CatalogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+/**
+ * @author 刘棋军
+ * @date2019-06-05
+ */
+
+@RequestMapping("/catalog")
+@Controller
+public class CatalogController {
+    @Autowired
+    CatalogService catalogService;
+    @RequestMapping("/queryChapters")
+    @ResponseBody
+    public List<Catalog> queryChapters(String video_id){
+        return catalogService.queryChapter(video_id);
+    }
+
+    @RequestMapping("/queryVideoCatalog")
+    @ResponseBody
+    public List<Catalog> queryVideoCatalog(String chapter_id){
+        return catalogService.queryVideoCatalog(chapter_id);
+    }
+
+}
