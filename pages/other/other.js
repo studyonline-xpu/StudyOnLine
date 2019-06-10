3abc648b8d8d9e4e4f4d26cde89727c81575561c
Lemon <1501901753@qq.com>
2019-06-10 17:08:45 +0800

    二级评论列表

--- pages/other/other.js
+++ pages/other/other.js
@@ -4,6 +4,7 @@ Page({
     currentIcon:"down.png",
     showModalStatus: false,
     videoUrl:'',
+    showModalStatus: false,
     current: '1',
     current_scroll: '1',
     tab1:true,
@@ -11,11 +12,13 @@ Page({
     tab3:false,
     name: 'name0',
     courseInfo: '',
+    conmentContent:'',
     good:[
       { name: "likes", value: 123 }, { name: "collection", value: 123 }, { name: "direct", value: 123}
     ],
     cataloge:[],
-    conment:[]
+    conment:[],
+    secondConment:[]
   },
   handleChange({ detail }) {
     var index = detail.key;
@@ -162,16 +165,111 @@ Page({
 
     }
   },
-  detailReply: function(e) {
-    var conmentId = e.currentTarget.dataset.fatherid;
+  getConment: function(e) {
+    this.setData({
+      conmentContent: e.detail.value
+    })
+  },
+  // 发布评论
+  release: function () {
+    var _this = this;
+    var conment = {};
+    conment.msg = _this.data.conmentContent;
+    conment.fromId = wx.getStorageSync("openid1");
+    conment.fatherId = _this.data.courseInfo.videoId;
+    conment.commentsId = '';
+    conment.toId = '';
+    conment.isFather = '';
+    conment.time = '';
+    conment.fromName = '';
+    conment.fromPicture = '';
+    var conmentString = JSON.stringify(conment);
     wx.request({
-      url: 'http://47.103.101.35:8080/study_online-manager-web/comments/queryComments',
+      url: 'http://47.103.101.35:8080/study_online-manager-web/comments/insertComments',
       data: {
-        father_id: conmentId,
+        commentsJson: conmentString
+      },
+      header: {
+        'content-type': 'application/x-www-form-urlencoded;charset=utf-8',
       },
       success: function(res) {
-        console.log(res)
+        _this.setData({
+          conmentContent: ''
+        })
       }
     })
+  },
+  // 二级评论动画
+  powerDrawer: function (e) {
+    var _this = this;
+    if (e.currentTarget.dataset.statu == 'open'){
+      var conmentId = e.currentTarget.dataset.fatherid;
+      wx.request({
+        url: 'http://47.103.101.35:8080/study_online-manager-web/comments/queryComments',
+        data: {
+          father_id: conmentId,
+        },
+        success: function (res) {
+          var list = res.data;
+          for (var i = 0; i < list.length; i++) {
+            var date = new Date(list[i].time);
+            var showDate = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate();
+            list[i].time = showDate;
+          }
+          _this.setData({
+            secondConment: list
+          })
+        }
+      })
+    }
+    var currentStatu = e.currentTarget.dataset.statu;
+    this.util(currentStatu)
+  },
+  util: function (currentStatu) {
+    /* 动画部分 */
+    // 第1步：创建动画实例 
+    var animation = wx.createAnimation({
+      duration: 200,  //动画时长
+      timingFunction: "linear", //线性
+      delay: 0  //0则不延迟
+    });
+
+    // 第2步：这个动画实例赋给当前的动画实例
+    this.animation = animation;
+
+    // 第3步：执行第一组动画：Y轴偏移240px后(盒子高度是240px)，停
+    animation.translateY(240).step();
+
+    // 第4步：导出动画对象赋给数据对象储存
+    this.setData({
+      animationData: animation.export()
+    })
+
+    // 第5步：设置定时器到指定时候后，执行第二组动画
+    setTimeout(function () {
+      // 执行第二组动画：Y轴不偏移，停
+      animation.translateY(0).step()
+      // 给数据对象储存的第一组动画，更替为执行完第二组动画的动画对象
+      this.setData({
+        animationData: animation
+      })
+
+      //关闭抽屉
+      if (currentStatu == "close") {
+        this.setData(
+          {
+            showModalStatus: false
+          }
+        );
+      }
+    }.bind(this), 200)
+    // 显示抽屉
+    if (currentStatu == "open") {
+      this.setData(
+        {
+          showModalStatus: true
+        }
+      );
+    }
   }
 })
