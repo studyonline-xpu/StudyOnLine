e66a9441e5a2ccd4baa63e40c57652311eb31bd6
LM123Code <1456466514@qq.com>
2019-06-13 09:02:29 +0800

    测试插入弹幕api

--- study_online-parent/study_online-manager-web/src/main/java/com/study_online/controller/BarrageController.java
+++ study_online-parent/study_online-manager-web/src/main/java/com/study_online/controller/BarrageController.java
@@ -22,12 +22,21 @@ public class BarrageController {
     BarrageService barrageService;
 
 
+    /**
+     * 根据目录id 查询弹幕
+     * @param catalog_id 目录id
+     * @return
+     */
     @RequestMapping("/queryBarrage")
     @ResponseBody
     public List<Barrage> queryBarrage(String catalog_id){
         return barrageService.queryBarrage(catalog_id);
     }
 
+    /**
+     * 插入弹幕
+     * @param barrage
+     */
     @RequestMapping("/insertBarrage")
     @ResponseBody
     public void insertBarrage(String barrage){
